40495128d273bfb38c16efdf3db24f00d0474644
doug revell <doug.revelljr@gmail.com>
2022-09-21 10:13:21 -0400

    new commit

---
+++ CalcData/Franchise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcData
+{
+    public class Franchise
+    {
+        public int FranchiseId { get; set; }
+        public string FranchiseName { get; set; }
+        public string State { get; set; }
+        public int Zips { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
---
+++ CalcData/Franchisee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcData
+{
+    public class Franchisee
+    {
+        [Key]
+        public int FranchiseeId { get; set; }
+        public string OwnerFirst { get; set; }
+        public string OwnerLast { get; set; }
+        public List<Franchise> Franchises {get; set;} 
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
